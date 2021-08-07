f3ad1303ed25befe8c04851ed3fe809c726a6b42
李少勇 <670504796@qq.com>
2021-08-07 16:29:27 +0800

    修改登录

--- miniprogram/pages/flashDetermination/index.js
+++ miniprogram/pages/flashDetermination/index.js
@@ -2,9 +2,11 @@
 const app = getApp()
 var plugin = requirePlugin("WechatSI")
 let manager = plugin.getRecordRecognitionManager()
+let _animation;
+let _animationIndex;
 Page({
   data: {
-    tabActive: '1',
+    tabActive: '2',
     // 会议开始结束时间
     meetDate: '',
     startTime: '',
@@ -32,12 +34,21 @@ Page({
   },
   onShow: function() {
     let that = this
+    // 初始化动画
+    _animation = wx.createAnimation({
+      duration: 500,
+      timingFunction: 'linear', // "linear","ease","ease-in","ease-in-out","ease-out","step-start","step-end"
+      delay: 0,
+      transformOrigin: '50% 50% 0'
+    })
+    // 初始化重力感应
     wx.onAccelerometerChange(function (e) {
       console.log('isshow', that.data.isShow)
       if (!that.data.isShow) {
         return
       }
       if (e.x > 1 && e.y > 1) {
+        this.startAnimationInterval()
         wx.showToast({
           title: '摇一摇成功',
           icon: 'success',
@@ -45,6 +56,36 @@ Page({
         })
       }
     })
+  },
+  /**
+  * 实现image旋转动画，每次旋转 120*n度
+  */
+  rotateAni: function (n) {
+    _animation.rotate(120 * (n)).step()
+    this.setData({
+      animation: _animation.export()
+    })
+  },
+
+  /**
+   * 开始旋转
+   */
+  startAnimationInterval: function () {
+  var that = this;
+    that.rotateAni(++_loadImagePathIndex); // 进行一次旋转
+      _animationIntervalId = setInterval(function () {
+        that.rotateAni(++_loadImagePathIndex);
+    },  500); // 没间隔_ANIMATION_TIME进行一次旋转
+  },
+
+  /**
+   * 停止旋转
+   */
+  stopAnimationInterval: function () {
+    if (_animationIntervalId> 0) {
+        clearInterval(_animationIntervalId);
+        _animationIntervalId = 0;
+    }
   },
   getNowDate: function(date){
     // return date.toLocaleDateString()
